d5a276749cf4f287cf1045955e6596d4e904efcf
Isleony <129456301+Isleony@users.noreply.github.com>
2024-04-24 14:43:13 -0300

    configurando o ugify

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,40 @@
-const gulp = require ('gulp');
+const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+const htmlmin = require('gulp-htmlmin');
 
-function styles(){
+function styles() {
     return gulp.src('./src/styles/*.scss')
-    .pipe(sass({outputStyle:'compressed'}))
-    .pipe(gulp.dest('./dist/css'));
+        .pipe(sass({
+            outputStyle: 'compressed' }))
+        .pipe(gulp.dest('./dist/css'))
 }
 
-exports.default = styles;
-exports.watch = function(){
+function images() {
+    return gulp.src('./src/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'))
+}
+
+function scripts() {
+    return gulp.src('./src/scripts/*.js') // Updated path
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'))
+}
+
+function htmlmini () {
+    return gulp.src('./src/*.html') // Updated path
+        .pipe(htmlmin({
+            collapseWhitespace: true,
+            collapseInlineTagWhitespace: true
+        }))
+        .pipe(gulp.dest('./dist'))
+}
+
+exports.default = gulp.parallel(styles, images, scripts, htmlmini);
+exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
+    gulp.watch('./src/*.html', gulp.parallel(htmlmini))
 }
