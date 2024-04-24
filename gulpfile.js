31007cf173133743d3fd9c559a2597a119b58042
Isleony <129456301+Isleony@users.noreply.github.com>
2024-04-24 19:02:09 -0300

    Corrigido o problema com as imagens

--- gulpfile.js
+++ gulpfile.js
@@ -7,46 +7,29 @@ const htmlmin = require('gulp-htmlmin');
 function styles() {
     return gulp.src('./src/styles/*.scss')
         .pipe(sass({
-            outputStyle: 'compressed'
-        }).on('error', sass.logError))  // Adicionado tratamento de erro para o Sass
+            outputStyle: 'compressed' }))
         .pipe(gulp.dest('./dist/css'))
-        .on('end', function() {
-            console.log('Estilos processados com sucesso!');
-        });
 }
 
 function images() {
-    return gulp.src('./src/images/**/*')
+    return gulp.src('./src/images/**/*.{jpg,png,gif,svg}')
         .pipe(imagemin())
-        .on('error', function(err) {
-            console.error(err.toString());
-            this.emit('end');
-        })
         .pipe(gulp.dest('./dist/images'))
-        .on('end', function() {
-            console.log('Imagens processadas com sucesso!');
-        });
 }
 
 function scripts() {
     return gulp.src('./src/scripts/*.js')
         .pipe(uglify())
         .pipe(gulp.dest('./dist/js'))
-        .on('end', function() {
-            console.log('Scripts processados com sucesso!');
-        });
 }
 
-function htmlmini() {
+function htmlmini () {
     return gulp.src('./src/*.html')
         .pipe(htmlmin({
             collapseWhitespace: true,
             collapseInlineTagWhitespace: true
         }))
         .pipe(gulp.dest('./dist'))
-        .on('end', function() {
-            console.log('HTML minificado com sucesso!');
-        });
 }
 
 exports.default = gulp.parallel(styles, images, scripts, htmlmini);
