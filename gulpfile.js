e31d7574cd1fa0259880a430425b57ef453f23ce
Isleony <129456301+Isleony@users.noreply.github.com>
2024-04-24 19:55:34 -0300

    Corrigido o problema com as imagens

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,57 @@
-const { src, dest, parallel, series, watch } = require('gulp');
+const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const imagemin = require('gulp-imagemin');
+const imagemin = import('gulp-imagemin');
 const uglify = require('gulp-uglify');
 const htmlmin = require('gulp-htmlmin');
 
 function styles() {
-    return src('./src/styles/*.scss')
-        .pipe(sass({
-            outputStyle: 'compressed'
-        }))
-        .pipe(dest('./dist/css'));
+  try {
+    return gulp.src('./src/styles/*.scss')
+      .pipe(sass({
+        outputStyle: 'compressed'
+      }))
+      .pipe(gulp.dest('./dist/css'));
+  } catch (error) {
+    console.error('Error compiling Sass:', error);
+  }
 }
 
 function images() {
+  try {
     return gulp.src('./src/images/**/*.{jpg,png,gif,svg,jpeg}')
-        .pipe(imagemin())
-        .on('error', function(err) {
-            console.error(err);
-            this.emit('end');
-        })
-        .pipe(gulp.dest('./dist/images'))
+      .pipe(imagemin())
+      .pipe(gulp.dest('./dist/images'));
+  } catch (error) {
+    console.error('Error optimizing images:', error);
+  }
 }
 
-
-
 function scripts() {
-    return src('./src/scripts/*.js')
-        .pipe(uglify())
-        .pipe(dest('./dist/js'));
+  try {
+    return gulp.src('./src/scripts/*.js')
+      .pipe(uglify())
+      .pipe(gulp.dest('./dist/js'));
+  } catch (error) {
+    console.error('Error minifying scripts:', error);
+  }
 }
 
 function htmlmini() {
-    return src('./src/*.html')
-        .pipe(htmlmin({
-            collapseWhitespace: true,
-            collapseInlineTagWhitespace: true
-        }))
-        .pipe(dest('./dist'));
+  try {
+    return gulp.src('./src/*.html')
+      .pipe(htmlmin({
+        collapseWhitespace: true,
+        collapseInlineTagWhitespace: true
+      }))
+      .pipe(gulp.dest('./dist'));
+  } catch (error) {
+    console.error('Error minifying HTML:', error);
+  }
 }
 
-exports.default = parallel(styles, images, scripts, htmlmini);
-
+exports.default = gulp.parallel(styles, images, scripts, htmlmini);
 exports.watch = function () {
-    watch('./src/styles/*.scss', styles);
-    watch('./src/scripts/*.js', scripts);
-    watch('./src/images/**/*.{jpg,png,gif,svg}', images);
-    watch('./src/*.html', htmlmini);
-}
+  gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+  gulp.watch('./src/scripts/*.js', gulp.parallel(scripts));
+  gulp.watch('./src/*.html', gulp.parallel(htmlmini));
+};
